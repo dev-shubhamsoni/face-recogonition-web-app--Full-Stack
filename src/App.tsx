dd76ee32cb50e8026de1d627ec411ae0d399a91a
dev-shubhamsoni <dev.shubhamsoni@gmail.com>
2023-12-17 19:07:52 +0530

    adding starting files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecogonition /> */}
+    </div>
+  )
+};
+
+export default App;
